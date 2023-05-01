125b53c72113bc54102f5a8c891b1d73a6d692ed
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-04-30 21:06:18 -0300

    feat: added post, patch and delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,5 +23,28 @@ export class CarsController {
   getById(@Param('id', ParseIntPipe) id: number) {
     console.log({ id });
     return this.carService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return {
+      status: 'New car created',
+      body,
+    };
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return {
+      status: `Car with id '${id}' updated`,
+      body,
+    };
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      status: `Car with id '${id}' deleted`,
+    };
   }
 }
