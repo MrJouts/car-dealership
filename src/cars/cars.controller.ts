5d974939f94e76d5f35af6f0df318b83b6332bc6
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-02 00:05:05 -0300

    feat: moved validationPipe to global config

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -8,14 +8,12 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
-  UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCartDto } from './dto/create-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carService: CarsService) {}
 
