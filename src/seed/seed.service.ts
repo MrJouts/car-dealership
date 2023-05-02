3929b64c02dba8ade6ea5fba7eba144e6a67dc58
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-02 18:39:25 -0300

    feat: updated seed service and README

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from 'src/cars/cars.service';
+
 import { BRANDS_SEED } from './data/brands.seed';
 import { CARS_SEED } from './data/car.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carService: CarsService,
+    private readonly brandService: BrandsService,
+  ) {}
+
   populateDB() {
-    //CARS_SEED
-    //BRANDS_SEED
+    this.carService.fillCarsWithSeedDate(CARS_SEED);
+    this.brandService.fillBrandsWithSeedDate(BRANDS_SEED);
 
     return 'SEED executed';
   }
